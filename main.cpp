cd5ce01bfe10fa9d2e67a3d92ccaf9a42621e68a
Merlina28 <113477440+Merlina28@users.noreply.github.com>
2022-11-18 12:31:07 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,57 +2,64 @@
 using namespace std;
 //transporte cuidades y carreteras
 
-struct Vertice{
+struct Vertice
+{
     string nombre;
 
     struct Vertice * sigV;//para enlazar todos los vertices en una lista
     struct Arco *subListaArcos;//representa los arcos que salen de ese vertice
 
     bool visitado;
-    Vertice (string nom){
-            nombre = nom;
-            sigV = NULL;
-            subListaArcos = NULL;
-            visitado = false;
+    Vertice (string nom)
+    {
+        nombre = nom;
+        sigV = NULL;
+        subListaArcos = NULL;
+        visitado = false;
     }
-}*vertice;
+};
 
-struct Arco{
+struct Arco
+{
     int distancia;
     string destino;
     struct Arco *sigA;//siguiente arco.
     struct Arco *antA;//anterior arco.
-    Arco(int dis, string des){
-            distancia = dis;
-            destino = des;
-            sigA = NULL;
-            antA = NULL;
+    Arco(int dis, string des)
+    {
+        distancia = dis;
+        destino = des;
+        sigA = NULL;
+        antA = NULL;
     }
 };
 
 struct Vertice *grafo;// el apuntador inicial del grafo
 
 //inserción al inicio de la lista de vertices.
-void insertarVertice(string nom){
-            struct Vertice *nuevoVertice = new Vertice(nom);
+void insertarVertice(string nom)
+{
+    struct Vertice *nuevoVertice = new Vertice(nom);
 
-            nuevoVertice->sigV = grafo;
-            grafo = nuevoVertice;
+    nuevoVertice->sigV = grafo;
+    grafo = nuevoVertice;
 }
 
-struct Vertice *   buscarVertice(string origen){
-        struct Vertice *tempV = grafo;
-        while(tempV != NULL){
-            if(tempV->nombre == origen)
-                return tempV;
+struct Vertice *   buscarVertice(string origen)
+{
+    struct Vertice *tempV = grafo;
+    while(tempV != NULL)
+    {
+        if(tempV->nombre == origen)
+            return tempV;
 
-            tempV = tempV->sigV;
-        }
+        tempV = tempV->sigV;
+    }
     return NULL;//no lo encontro
 }
 
 //Funcion para vertices, modifica el nombre
-bool modificar(string nombre)
+bool modificarVertice(string nombre, string nuevoNombre)
 {
     Vertice * tempV = buscarVertice(nombre);
 
@@ -70,33 +77,33 @@ bool modificar(string nombre)
 }
 
 //Funcion que permite eliminar un vertice
-bool eliminarVertice(string nom)
+bool eliminarVertice(string nom, Vertice * sigV)
 {
-    if(vertice == NULL)
+    if(sigV == NULL)
     {
         cout <<"\nLista vacia";
         return false;
     }
-    else if(vertice->nombre== nom)
+    else if(sigV->nombre== nom)
     {
-        vertice = vertice->sig;
+        sigV = sigV->sigV;
         return true;
     }
     else
     {
-        Vertice *temp = vertice;
-        Vertice *tempAnt= vertice;
+        Vertice *temp = sigV;
+        Vertice *tempAnt= sigV;
         while(temp != NULL)
         {
 
             if(temp->nombre == nom) //borrar
             {
-                tempAnt->sig  = temp->sig;
+                tempAnt->sigV  = temp->sigV;
                 return true;
             }
 
             tempAnt= temp;
-            temp = temp->sig;
+            temp = temp->sigV;
         }
 
         if(temp==NULL)
@@ -108,46 +115,45 @@ bool eliminarVertice(string nom)
     }
 }
 
-void insertarArco(string origen, int dis, string des){
+void insertarArco(string origen, int dis, string des)
+{
 
-        struct Vertice *vOrigen = buscarVertice(origen);
-        struct Vertice *vDestino = buscarVertice(des);
+    struct Vertice *vOrigen = buscarVertice(origen);
+    struct Vertice *vDestino = buscarVertice(des);
 
-        if(vOrigen == NULL){
-            cout<<"\nNo se encuentra el origen.";
-            return;
-        }
-        if(vDestino == NULL){
-            cout<<"\nNo se encuentra el destino.";
-            return;
-        }
-        struct Arco *nuevoArco = new Arco(dis,des);
+    if(vOrigen == NULL)
+    {
+        cout<<"\nNo se encuentra el origen.";
+        return;
+    }
+    if(vDestino == NULL)
+    {
+        cout<<"\nNo se encuentra el destino.";
+        return;
+    }
+    struct Arco *nuevoArco = new Arco(dis,des);
 
-        //se inserto al inicio de la sublista de Arcos
-        nuevoArco->sigA = vOrigen->subListaArcos;
-        nuevoArco->antA = NULL
-        vOrigen->subListaArcos = nuevoArco;
+    //se inserto al inicio de la sublista de Arcos
+    nuevoArco->sigA = vOrigen->subListaArcos;
+    nuevoArco->antA = NULL;
+    vOrigen->subListaArcos = nuevoArco;
 }
 
 bool buscarArco(int dis, string des, Arco* subListaArcos)
 {
-
     Arco*temp = subListaArcos;
     while(temp != NULL)
     {
-
-        if(dis == temp->num && des == temp-> dest)
+        if(dis == temp->dis && des == temp-> des)
             return true;
+            return temp;
         temp = temp->sig;
     }
     return false;
+    return NULL;
 }
-
-return false;
-
-void modificar(int x, int nuevoValor, nodo*lista){
-
-
+void modificar(int x, int nuevoValor, nodo*lista)
+{
     nodo* modif = buscarArco(x, lista);
     if(modif!= NULL)
         modif->num = nuevoValor;
@@ -191,154 +197,173 @@ bool eliminarArco(int id)
     }
 }
 
-void cargarDatos(){
-        insertarVertice("A");
-        insertarVertice("B");
-        insertarVertice("C");
-        insertarVertice("D");
-        insertarVertice("E");
+void cargarDatos()
+{
+    insertarVertice("A");
+    insertarVertice("B");
+    insertarVertice("C");
+    insertarVertice("D");
+    insertarVertice("E");
 
-        insertarArco("A",12,"B");
-        insertarArco("B",12,"C");
+    insertarArco("A",12,"B");
+    insertarArco("B",12,"C");
 
-        insertarArco("B",6,"C");
-        insertarArco("C",6,"B");
+    insertarArco("B",6,"C");
+    insertarArco("C",6,"B");
 
-        insertarArco("B",14,"F");
-        insertarArco("F",14,"B");
+    insertarArco("B",14,"F");
+    insertarArco("F",14,"B");
 
-        insertarArco("C",8,"D");
-        insertarArco("D",8,"C");
+    insertarArco("C",8,"D");
+    insertarArco("D",8,"C");
 
-        insertarArco("D",10,"E");
-        insertarArco("E",10,"D");
+    insertarArco("D",10,"E");
+    insertarArco("E",10,"D");
 }
 
+void amplitud()
+{
+    struct Vertice *tempV = grafo;
 
+    while(tempV != NULL) //RECORRE LA LISTA DE VERTICES
+    {
+        cout<<"\nVertice:  "<<tempV->nombre<<"\t";
+        struct Arco *tempA = tempV->subListaArcos;
 
-void amplitud(){
-        struct Vertice *tempV = grafo;
-
-        while(tempV != NULL){//RECORRE LA LISTA DE VERTICES
-            cout<<"\nVertice:  "<<tempV->nombre<<"\t";
-            struct Arco *tempA = tempV->subListaArcos;
-
-            while(tempA != NULL){//RECORRE LOS ARCOS DE LA LISTA DE ARCOS DEL VERTICE
-                    cout<<tempV->nombre<<tempA->destino<<tempA->distancia<<",  ";
-                tempA = tempA->sigA;
-            }
-            tempV = tempV->sigV;
-        }
-}
-
-void profundidad (struct Vertice *inicio){
-        if((inicio == NULL) or (inicio->visitado== true)){
-                cout<<endl;
-            return;
-        }
-
-        inicio->visitado = true;
-
-        struct Arco *tempA = inicio->subListaArcos;
-        while(tempA != NULL){
-            cout<<inicio->nombre<<tempA->destino<<tempA->distancia<<",  ";
-
-            profundidad(buscarVertice(tempA->destino));
-
+        while(tempA != NULL) //RECORRE LOS ARCOS DE LA LISTA DE ARCOS DEL VERTICE
+        {
+            cout<<tempV->nombre<<tempA->destino<<tempA->distancia<<",  ";
             tempA = tempA->sigA;
         }
-
+        tempV = tempV->sigV;
+    }
 }
 
-void desmarcar(){
-        struct Vertice *tempV = grafo;
+void profundidad (struct Vertice *inicio)
+{
+    if((inicio == NULL) or (inicio->visitado== true))
+    {
+        cout<<endl;
+        return;
+    }
 
-        while(tempV != NULL){
+    inicio->visitado = true;
 
-            tempV->visitado = false;
+    struct Arco *tempA = inicio->subListaArcos;
+    while(tempA != NULL)
+    {
+        cout<<inicio->nombre<<tempA->destino<<tempA->distancia<<",  ";
 
-            tempV = tempV->sigV;
-        }
+        profundidad(buscarVertice(tempA->destino));
+
+        tempA = tempA->sigA;
+    }
+}
+
+void desmarcar()
+{
+    struct Vertice *tempV = grafo;
+
+    while(tempV != NULL)
+    {
+
+        tempV->visitado = false;
+
+        tempV = tempV->sigV;
+    }
 }
 
 bool existeRuta = false;
-bool buscarRuta( struct Vertice *origen, string destino){
+bool buscarRuta( struct Vertice *origen, string destino)
+{
 
     if((origen == NULL) or (origen->visitado== true))
         return existeRuta;
 
-        if(origen->nombre == destino){
-                existeRuta= true;
-                 return existeRuta;
-        }
-        origen->visitado =true;
+    if(origen->nombre == destino)
+    {
+        existeRuta= true;
+        return existeRuta;
+    }
+    origen->visitado =true;
 
     struct Arco *tempA =origen->subListaArcos;
-    while(tempA != NULL){
+    while(tempA != NULL)
+    {
 
         buscarRuta(buscarVertice(tempA->destino), destino);
         tempA = tempA->sigA;
     }
 }
 
-bool imprimirRuta(struct Vertice *origen, string destino, string ruta){
+bool imprimirRuta(struct Vertice *origen, string destino, string ruta)
+{
     if((origen == NULL) or (origen->visitado== true))
-            return existeRuta;
+        return existeRuta;
 
-            if(origen->nombre == destino){
-                    cout<<"\n\nRuta................  "<<ruta<<destino;
-                    existeRuta= true;
-                     return existeRuta;
-            }
-            origen->visitado =true;
+    if(origen->nombre == destino)
+    {
+        cout<<"\n\nRuta................  "<<ruta<<destino;
+        existeRuta= true;
+        return existeRuta;
+    }
+    origen->visitado =true;
 
-        struct Arco *tempA =origen->subListaArcos;
-        while(tempA != NULL){
+    struct Arco *tempA =origen->subListaArcos;
+    while(tempA != NULL)
+    {
 
-            imprimirRuta(buscarVertice(tempA->destino), destino,ruta+origen->nombre);
-            tempA = tempA->sigA;
-        }
-         origen->visitado =false;
+        imprimirRuta(buscarVertice(tempA->destino), destino,ruta+origen->nombre);
+        tempA = tempA->sigA;
+    }
+    origen->visitado =false;
 }
 
-bool imprimirRutaconDistancias(struct Vertice *origen, string destino, string ruta, int dis){
+bool imprimirRutaconDistancias(struct Vertice *origen, string destino, string ruta, int dis)
+{
     if((origen == NULL) or (origen->visitado== true))
-            return existeRuta;
+        return existeRuta;
 
-            if(origen->nombre == destino){
-                    cout<<"\nLa ruta es: "<<ruta<<destino<<" La distancia es: "<<dis;
-                    existeRuta= true;
-                     return existeRuta;
-            }
-            origen->visitado =true;
+    if(origen->nombre == destino)
+    {
+        cout<<"\nLa ruta es: "<<ruta<<destino<<" La distancia es: "<<dis;
+        existeRuta= true;
+        return existeRuta;
+    }
+    origen->visitado =true;
 
-        struct Arco *tempA =origen->subListaArcos;
-        while(tempA != NULL){
-            imprimirRutaconDistancias(buscarVertice(tempA->destino), destino,
-                                      ruta+origen->nombre, dis + tempA->distancia);
-            tempA = tempA->sigA;
-        }
-        origen->visitado =false;
+    struct Arco *tempA =origen->subListaArcos;
+    while(tempA != NULL)
+    {
+        imprimirRutaconDistancias(buscarVertice(tempA->destino), destino,
+                                  ruta+origen->nombre, dis + tempA->distancia);
+        tempA = tempA->sigA;
+    }
+    origen->visitado =false;
 }
 //variables globales
 string rutaMenor = "";
 int distanciaMenor = 0;
-bool rutaCorta(struct Vertice *origen, string destino, string ruta, int dis){
+bool rutaCorta(struct Vertice *origen, string destino, string ruta, int dis)
+{
     if((origen == NULL) or (origen->visitado== true))
         return existeRuta;
 
-        if(origen->nombre == destino){
-                if((distanciaMenor==0) || (dis < distanciaMenor)){
-                        distanciaMenor =dis;
-                        rutaMenor = ruta+destino;
-                }
-                existeRuta= true;
-                 return existeRuta;
+    if(origen->nombre == destino)
+    {
+        if((distanciaMenor==0) || (dis < distanciaMenor))
+        {
+            distanciaMenor =dis;
+            rutaMenor = ruta+destino;
         }
-        origen->visitado =true;
+        existeRuta= true;
+        return existeRuta;
+    }
+    origen->visitado =true;
 
     struct Arco *tempA =origen->subListaArcos;
-    while(tempA != NULL){
+    while(tempA != NULL)
+    {
 
         rutaCorta(buscarVertice(tempA->destino), destino, ruta+origen->nombre, dis + tempA->distancia);
         tempA = tempA->sigA;
@@ -451,4 +476,5 @@ int main()
         }
     return 0;
 }
+
 
