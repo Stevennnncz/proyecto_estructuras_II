158888bdd1c542f64438a36d2d3243df8646a430
Merlina28 <113477440+Merlina28@users.noreply.github.com>
2022-11-20 17:40:50 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -47,17 +47,16 @@ struct Persona
     int minCaminata;
     Persona *sig;
 
-
     struct Amigos *sublista;
 
-    Persona(string n, int FA, string VI, int ind,int num, int minutos)
+    Persona(string n, int FA, string VI, int ind,int num, int minC)
     {
         nombre = n;
         verticeInicial = VI;
         formaDeAvance = FA;
         id = ind;
+        minCaminata=minC;
         NumeroAmigos = num;
-        minCaminata = minutos;
         sublista = NULL;
         sig = NULL;
     }
@@ -86,11 +85,11 @@ struct Persona*BuscarPersona(string Name)
 }
 
 //Funcion que inserta una nueva persona a la lista
-void insertarPersona(string name, int tipoAvanze, string verticeI,int id,int n,int minutos)
+void insertarPersona(string name, int tipoAvanze, string verticeI,int id,int n, int minC)
 {
     if(BuscarPersona(name) == NULL)
     {
-        Persona*nn = new Persona(name,tipoAvanze,verticeI,id, n,minutos);
+        Persona*nn = new Persona(name,tipoAvanze,verticeI,id, n, minC);
         nn->sig = lPersonas;
         lPersonas == nn;
     }
@@ -99,6 +98,7 @@ void insertarPersona(string name, int tipoAvanze, string verticeI,int id,int n,i
         cout<<"La persona ya se encuenta en la lista.";
     }
 }
+
 //Funcion que elimina una persona de la lista
 bool eliminarPersona(string name)
 {
@@ -390,14 +390,13 @@ struct Arco* buscarArco(string origen, int ti, string des)
         if(vDestino == NULL)
         {
             cout<<"\nNo se encuentra el destino.";
-
         }
         return temp;
         temp = temp->sigA;
 
     }
 }
-void modificar(int ti,int nuevoti, string nuevodes, string des, Arco*lista, string origen)
+void modificarArco(int ti,int nuevoti, string nuevodes, string des, Arco*lista, string origen)
 {
 
     Arco* modif = buscarArco(origen,ti,des);
@@ -449,6 +448,30 @@ bool eliminarArco(int ti, string des, string origen)
     }
 }
 
+void imprimirArco(int ti, string des, string origen)
+{
+    struct Vertice *vOrigen = buscarVertice(origen);
+    struct Arco *arc = vOrigen->subListaArcos;
+    if(vOrigen== NULL)
+        cout<<"\nNo existe el vertice.";
+    else
+    {
+        if(arc== NULL)
+        {
+            cout<<"No se encontro arcos";
+        }
+        else
+        {
+            cout<<"Los arcos del vertice son: "<<endl;
+            while(arc != NULL)
+            {
+                cout<<arc->tiempo<<arc->destino<<endl;
+                arc = arc->sigA;
+            }
+        }
+    }
+}
+
 void cargarDatos()
 {
   //Personas
@@ -473,7 +496,7 @@ void cargarDatos()
     insertarPersona("Lucía",2,"B",1289,0,0);
     insertarPersona("Gerardo",1,"A",3283,0,0);
     insertarPersona("Johana",4,"D",8672,0,0);
-    
+
     //Vertices
     insertarVertice("A");
     insertarVertice("B");
@@ -649,11 +672,11 @@ bool rutaCorta(struct Vertice *origen, string destino, string ruta, int dis)
 }
 
 
-//Funcion que imprime quien fue mas rapido en completar su ruta (⓿_⓿) o quien fue el mas lento  {{{(>_<)}}}
-void primerPuesto(Persona* lista, string Quchao){ //el estring sirve para ver si retorna el mas rapido o el mas lento
+//Funcion que imprime quien fue mas rapido en completar su ruta  o quien fue el mas lento
+void primerPuesto(Persona* lista, string Quchao){ //el string sirve para ver si retorna el mas rapido o el mas lento
     Persona*tem = lista;
-    int franshescoVirgolini = 0;  //SOY FRANCESCO VIRGOLINI, LA MÁQUINA MÁS VELOZ DE TOTA ITALIA! 🥵🥵
-    int flashZootopia = 0;          //XDN't? 🦀🧀
+    int franshescoVirgolini = 0;
+    int flashZootopia = 0;
 
 
     //comprueba quien tardo mas minutos
@@ -708,7 +731,6 @@ void primerPuesto(Persona* lista, string Quchao){ //el estring sirve para ver si
     }
 }
 
-
 //Funcion que imprime toda la informacion que hay en la lista de personas
 void Doxeados(Persona*lista){
     Persona*tem = lista;
@@ -722,7 +744,7 @@ void Doxeados(Persona*lista){
         cout<<"Su forma de avance durante la caminata: "<<tem->formaDeAvance<<endl;
         cout<<"Numero de amigos que hizo en su caminata: "<<tem->NumeroAmigos<<endl;
         if (tem->NumeroAmigos > 0) {
-            cout<<"Amigos que hizo (Nombre y ID) (⓿_⓿): "<<endl;
+            cout<<"Amigos que hizo (Nombre y ID): "<<endl;
             Amigos*sublistatem = tem->sublista;
             while (tem->sublista != NULL) {
                 cout<<"Nombre: "<<sublistatem->enlace->nombre<<endl;
@@ -734,8 +756,7 @@ void Doxeados(Persona*lista){
     }
 }
 
-
-//¯\_(ツ)_/¯ pues lo que dice el nombre la funcion ¯\_(ツ)_/¯
+//MENU
 int main()
 {
     cargarDatos();
@@ -752,7 +773,7 @@ int main()
 
         cout<<"Ingrese 3 para crear, modificar o borrar una persona"<<endl;
 
-        cout<<"Ingrese 4 para imprimir los arcos, vertices o personas"<<endl;
+        cout<<"Ingrese 4 para imprimir los arcos de un vertice, vertices o personas"<<endl;
 
         cout<<"Ingrese 5 para imprimir el estado de las personas en el avance"<<endl;
 
@@ -772,7 +793,7 @@ int main()
 
         cout<<"Ingrese 13 para imprimir cuales personas no encontraron algun amigo"<<endl;
 
-        cout<<"Ingrese 14 para imprimir cuales no pudieron realizar la caminata al no existir ruta";
+        cout<<"Ingrese 14 para imprimir cuales no pudieron realizar la caminata al no existir ruta"<<endl;
 
         cout<<"Ingrese 0 para salir"<<endl;
 
@@ -789,11 +810,11 @@ int main()
             ope = -1;
             while(ope != 0)
             {
-                cout<<"Ingrese 1 si desea crear un vertice";
+                cout<<"Ingrese 1 si desea crear un vertice"<<endl;
 
-                cout<<"Ingrese 2 si desea modificar un vertice";
+                cout<<"Ingrese 2 si desea modificar un vertice"<<endl;
 
-                cout<<"Ingrese 3 si desea borrar un vertice";
+                cout<<"Ingrese 3 si desea borrar un vertice"<<endl;
 
                 cout<<"Ingrese 0 para salir"<<endl;
 
@@ -802,9 +823,29 @@ int main()
                 cin  >> ope;
                 cout<<endl;
 
-                if(op == 1)
+                if(ope == 1)
                 {
-
+                    string no;
+                    cout<<"Ingrese el nombre del vertice que desea incertar: ";
+                    cin>>no;
+                    insertarVertice(no);
+                }
+                if(ope == 2)
+                {
+                    string n;
+                    string no;
+                    cout<<"Ingrese el nombre del vertice:";
+                    cout<<"Ingrese el nuevo nombre del vertice:";
+                    cin>>no;
+                    cin>>n;
+                    modificarVertice(n,no);
+                }
+                if(ope == 3)
+                {
+                    string no;
+                    cout<<"Ingrese el nombre del vertice:";
+                    cin>>no;
+                     eliminarVertice(no, Vertice);
                 }
             }
         }
@@ -814,11 +855,11 @@ int main()
             oper = -1;
             while(oper != 0)
             {
-                cout<<"Ingrese 1 si desea crear un arco";
+                cout<<"Ingrese 1 si desea crear un arco"<<endl;
 
-                cout<<"Ingrese 2 si desea modificar un arco";
+                cout<<"Ingrese 2 si desea modificar un arco"<<endl;
 
-                cout<<"Ingrese 3 si desea borrar un arco";
+                cout<<"Ingrese 3 si desea borrar un arco"<<endl;
 
                 cout<<"Ingrese 0 para salir"<<endl;
 
@@ -838,11 +879,11 @@ int main()
                 opera = -1;
                 while(opera != 0)
                 {
-                    cout<<"Ingrese 1 si desea crear una persona";
+                    cout<<"Ingrese 1 si desea crear una persona"<<endl;
 
-                    cout<<"Ingrese 2 si desea modificar una persona";
+                    cout<<"Ingrese 2 si desea modificar una persona"<<endl;
 
-                    cout<<"Ingrese 3 si desea borrar una persona";
+                    cout<<"Ingrese 3 si desea borrar una persona"<<endl;
 
                     cout<<"Ingrese 0 para salir"<<endl;
 
@@ -856,3 +897,4 @@ int main()
         }
     }
 }
+
